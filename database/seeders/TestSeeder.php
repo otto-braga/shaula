f0c5f50dad1cfbfa88e0e9867cbfc1e339217965
Otto Braga <otto.braga@gmail.com>
2025-05-12 17:31:16 +0000

    Implemented MentionSeeder.

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -23,5 +23,6 @@ class TestSeeder extends Seeder
         $this->call(ReviewSeeder::class);
         $this->call(HistoryArticleSeeder::class);
         $this->call(SourceSeeder::class);
+        $this->call(MentionSeeder::class);
     }
 }
