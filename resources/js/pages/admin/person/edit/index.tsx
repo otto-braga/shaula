b26d38190f9399bed6c958bf579e92f290d8bbda
Otto Braga <otto.braga@gmail.com>
2025-05-14 17:37:05 +0000

    Fix optional chaining for links and chronology initial values in editor components

--- resources/js/pages/admin/person/edit/index.tsx
+++ resources/js/pages/admin/person/edit/index.tsx
@@ -188,7 +188,7 @@ export default function Index({
                                     tinymceScriptSrc='/tinymce/tinymce.min.js'
                                     licenseKey='gpl'
                                     onInit={(_evt, editor) => editorLinksRef.current = editor}
-                                    initialValue={person.data.links as string || String()}
+                                    initialValue={person?.data.links as string || String()}
                                     init={{
                                         plugins: [
                                             'advlist', 'autolink', 'lists', 'link', 'charmap',
@@ -229,7 +229,7 @@ export default function Index({
                                     tinymceScriptSrc='/tinymce/tinymce.min.js'
                                     licenseKey='gpl'
                                     onInit={(_evt, editor) => editorChronologyRef.current = editor}
-                                    initialValue={person.data.chronology as string || String()}
+                                    initialValue={person?.data.chronology as string || String()}
                                     init={{
                                         plugins: [
                                             'advlist', 'autolink', 'lists', 'link', 'charmap',
