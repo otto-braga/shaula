77a8b1d5846b62bc9e9d2b00432e514beb4b0989
otto-braga <otto.braga@gmail.com>
2025-07-16 18:26:50 +0000

    Added responsiveness to Review edit pages.

--- resources/js/pages/admin/review/edit/images.tsx
+++ resources/js/pages/admin/review/edit/images.tsx
@@ -1,9 +1,9 @@
-import AppLayout from '@/layouts/app-layout';
 import { Review } from '@/types/review';
-import { Head, useForm, usePage } from '@inertiajs/react';
+import { useForm, usePage } from '@inertiajs/react';
 import { FormEventHandler } from 'react';
 import EditTabs from '@/components/edit/edit-tabs';
 import EditImages from '@/components/edit/edit-images';
+import EditLayout from '@/components/edit/edit-layout';
 
 export default function Images({
     review,
@@ -27,31 +27,23 @@ export default function Images({
     };
 
     return (
-        <AppLayout>
-            <Head title="Produções" />
-            <section className="px-4 py-12 text-gray-800 dark:text-gray-200">
-                <div className="mx-auto lg:px-8">
-                    <div className="">
-                        <form onSubmit={submit} className="space-y-6 bg-inherit">
-                            <EditTabs
-                                model={review}
-                                route_base_name="reviews"
-                                processing={processing}
-                            />
-
-                            <EditImages
-                                stored_images={review.data.images}
-                                stored_primary_image_uuid={review.data.primary_image?.uuid}
-                                data={data}
-                                setData={setData}
-                                errors={errors}
-                                processing={processing}
-                            />
-                        </form>
-                    </div>
-                </div>
-            </section>
-        </AppLayout>
+        <EditLayout>
+            <form onSubmit={submit} className="space-y-6 bg-inherit">
+                <EditTabs
+                    model={review}
+                    route_base_name="reviews"
+                    processing={processing}
+                />
+                <EditImages
+                    stored_images={review.data.images}
+                    stored_primary_image_uuid={review.data.primary_image?.uuid}
+                    data={data}
+                    setData={setData}
+                    errors={errors}
+                    processing={processing}
+                />
+            </form>
+        </EditLayout>
     );
 }
 
--- resources/js/pages/admin/review/edit/index.tsx
+++ resources/js/pages/admin/review/edit/index.tsx
@@ -1,14 +1,14 @@
 import InputError from '@/components/input-error';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import AppLayout from '@/layouts/app-layout';
 import { Review } from '@/types/review';
-import { Head, useForm, usePage } from '@inertiajs/react';
+import { useForm, usePage } from '@inertiajs/react';
 import { FormEventHandler } from 'react';
 import EditTabs from '@/components/edit/edit-tabs';
 import { LazyLoadingSelectWithStates } from '@/components/select/lazy-loading-select';
 import { MultiValue } from 'react-select';
 import { SearchResult } from '@/types/search-result';
+import EditLayout from '@/components/edit/edit-layout';
 
 // type DateWithTimezone =
 
@@ -47,74 +47,67 @@ export default function Index({
     };
 
     return (
-        <AppLayout>
-            <Head title="Produções" />
-            <section className="px-4 py-12 text-gray-800 dark:text-gray-200">
-                <div className="mx-auto lg:px-8">
-                    <div className="">
-                        <form onSubmit={submit} className="space-y-3 bg-inherit">
-                            <EditTabs
-                                model={review}
-                                route_base_name="reviews"
-                                processing={processing}
-                            />
+        <EditLayout>
+            <form onSubmit={submit} className="space-y-3 bg-inherit">
+                <EditTabs
+                    model={review}
+                    route_base_name="reviews"
+                    processing={processing}
+                />
 
-                            {isEdit}
+                {isEdit}
 
-                            <div>
-                                <Label htmlFor="title">Título</Label>
-                                <Input id="title" value={data.title ?? ''} onChange={(e) => setData('title', e.target.value)} autoComplete="title" />
-                                <InputError className="mt-2" message={errors.title} />
-                            </div>
+                <div>
+                    <Label htmlFor="title">Título</Label>
+                    <Input id="title" value={data.title ?? ''} onChange={(e) => setData('title', e.target.value)} autoComplete="title" />
+                    <InputError className="mt-2" message={errors.title} />
+                </div>
 
-                            <div>
-                                <Label htmlFor="authors_uuids">Autores</Label>
-                                <LazyLoadingSelectWithStates
-                                    isMulti
-                                    routeName={'people.fetch.options'}
-                                    value={review?.data.authors?.map(
-                                        author => ({ uuid: author.uuid, label: author.name })
-                                    )}
-                                    onChange={(options: MultiValue<SearchResult>) => {
-                                        setData('authors_uuids', options.map((option) => (option.uuid)))
-                                    }}
-                                />
-                                <InputError className="mt-2" message={errors.authors_uuids} />
-                            </div>
+                <div>
+                    <Label htmlFor="authors_uuids">Autores</Label>
+                    <LazyLoadingSelectWithStates
+                        isMulti
+                        routeName={'people.fetch.options'}
+                        value={review?.data.authors?.map(
+                            author => ({ uuid: author.uuid, label: author.name })
+                        )}
+                        onChange={(options: MultiValue<SearchResult>) => {
+                            setData('authors_uuids', options.map((option) => (option.uuid)))
+                        }}
+                    />
+                    <InputError className="mt-2" message={errors.authors_uuids} />
+                </div>
 
-                            <div className="flex flex-row gap-3">
-                                <div className="w-full">
-                                    <Label htmlFor="date">Data</Label>
-                                    <Input
-                                        id="date"
-                                        type="date"
-                                        value={data.date}
-                                        onChange={(e) => setData('date', e.target.value)}
-                                        autoComplete="date"
-                                        className="w-full"
-                                    />
-                                    <InputError className="mt-2" message={errors.date} />
-                                </div>
-                            </div>
-
-                            <div>
-                                <Label htmlFor="categories">Categorias</Label>
-                                <LazyLoadingSelectWithStates
-                                    isMulti
-                                    routeName={'categories.fetch.options'}
-                                    value={review?.data.categories?.map(
-                                        category => ({ uuid: category.uuid, label: category.name })
-                                    )}
-                                    onChange={(options: MultiValue<SearchResult>) => {
-                                        setData('categories_uuids', options.map((option) => (option.uuid)))
-                                    }}
-                                />
-                                <InputError className="mt-2" message={errors.categories_uuids} />
-                            </div>
-                        </form>
+                <div className="flex flex-row gap-3">
+                    <div className="w-full">
+                        <Label htmlFor="date">Data</Label>
+                        <Input
+                            id="date"
+                            type="date"
+                            value={data.date}
+                            onChange={(e) => setData('date', e.target.value)}
+                            autoComplete="date"
+                            className="w-full"
+                        />
+                        <InputError className="mt-2" message={errors.date} />
                     </div>
                 </div>
-            </section>
-        </AppLayout>
+
+                <div>
+                    <Label htmlFor="categories">Categorias</Label>
+                    <LazyLoadingSelectWithStates
+                        isMulti
+                        routeName={'categories.fetch.options'}
+                        value={review?.data.categories?.map(
+                            category => ({ uuid: category.uuid, label: category.name })
+                        )}
+                        onChange={(options: MultiValue<SearchResult>) => {
+                            setData('categories_uuids', options.map((option) => (option.uuid)))
+                        }}
+                    />
+                    <InputError className="mt-2" message={errors.categories_uuids} />
+                </div>
+            </form>
+        </EditLayout>
     );
 }
