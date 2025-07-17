9c7b2f1f77a33fea9c1fc12205f7beb06dcf15ab
otto-braga <otto.braga@gmail.com>
2025-07-17 19:46:39 +0000

    test

--- config/filesystems.php
+++ config/filesystems.php
@@ -91,7 +91,7 @@ return [
 
     'links' => [
         // public_path('storage') => storage_path('app/public'),
-        public_path('storage') => storage_path('app/..'),
+        public_path('storage') => storage_path('..'),
     ],
 
 ];
