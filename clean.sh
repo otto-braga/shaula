31bba1ea239898e0e42d3eec6e6d7fd22ee328e6
otto-braga <otto.braga@gmail.com>
2025-07-19 03:53:59 -0300

    test

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf .env
+rm -rf .env.example
+
+rm -rf .git
+rm -rf .github
+rm -rf .gitattributes
+rm -rf .gitignore
+
+rm -rf .docker
+rm -rf .dockerignore
+rm -rf docker-compose.yml
+rm -rf Dockerfile
+
+rm -rf optimize.sh
