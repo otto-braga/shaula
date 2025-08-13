26e714de4cc245a179f1b8c9cfbb89b44e3ef930
danilo-santos <danilo.andrade333@gmail.com>
2025-08-13 20:28:41 -0300

    criado instance profile que autoriza ec2 a ler ecr

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,3 +60,32 @@ resource "aws_security_group" "webserver" {
     description = "All outbound traffic"
   }
 }
+
+resource "aws_iam_role" "ecr_read_only_role" {
+  name = "ec2-role"
+
+  # Política de confiança que permite que o serviço EC2 assuma esta role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read_only_attachment" {
+  role       = aws_iam_role.ecr_read_only_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ecr_read_only_role.name
+}
