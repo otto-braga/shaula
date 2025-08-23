428566b68fc1fc30f9df90e02bb42586a7df2004
danilo-santos <danilo.andrade333@gmail.com>
2025-08-23 12:18:09 -0300

    teste com ecr final

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -11,7 +11,8 @@ export default function Index({ reviews, artworks }: { reviews: { data: Review[]
             <section className="px-4 py-8 md:p-8">
                 <Link href="/critica" className="inline-block">
                     <span className="flex items-center hover:underline">
-                        <h2 className="text-3xl text-red">ÚLTIMAS CRÍTICAS -TESTE3</h2>
+                        <h2 className="text-red text-3xl">ÚLTIMAS CRÍTICAS -TESTE 4</h2>
+                        <h2 className="text-red-600">teste com ECR</h2>
                         <ArrowUpRight size={32} />
                     </span>
                 </Link>
